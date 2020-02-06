1dc671130a37c0e4b8cd7e7c9290757a6f36115b
Jonathan Logan Friedman <palblet@yahoo.com>
2020-02-06 17:05:58 -0600

    Displayed set of Books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,9 +1,28 @@
 package com.twu.biblioteca;
 
+import com.sun.codemodel.internal.JArray;
+
+import java.util.ArrayList;
+
 public class BibliotecaApp {
+    static ArrayList<String> list = new ArrayList<String>();
 
     public static void main(String[] args) {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        populateBookList();
+        displayBookList();
+
     }
 
+    public static void populateBookList(){
+        list.add("Book One");
+        list.add("Book Two");
+        list.add("Book Three");
+    }
+
+    public static void displayBookList(){
+        for (String i:list) {
+            System.out.println(i);
+        }
+    }
 }
