6c941ccff4ddd6cbb2018fa47e0c654e8ac5dcdc
Jonathan Logan Friedman <palblet@yahoo.com>
2020-02-07 09:58:26 -0600

    Added Checking in a Book

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class BibliotecaApp {
 
     private static ArrayList<Book> list = new ArrayList<Book>();
-    private static int numMenuItems = 3;
+    private static int numMenuItems = 4;
     private static int numOfBooks = 3;
 
     public static void main(String[] args) {
@@ -23,10 +23,13 @@ public class BibliotecaApp {
             space();
 
             switch(getInput(numMenuItems)){
-                case 3: done = true; break;
-                case 1: displayBookList(); break;
+                case 4: done = true;break;
+                case 1: displayBookList();break;
                 case 2: checkOutBook();break;
+                case 3: checkInBook();break;
             }
+
+            space();
         }
     }
 
@@ -34,48 +37,57 @@ public class BibliotecaApp {
         System.out.println("Menu:\n"+
                 "1: List of Books\n"+
                 "2: Check out a Book\n"+
-                "3: Exit");
+                "3: Check in a Book\n"+
+                "4: Exit");
     }
 
     public static int getInput(int num) {
         Scanner scan = new Scanner(System.in);
         String input;
         do{
-            System.out.println("Please enter selection: ");
+            System.out.print("Please enter selection: ");
             input = scan.nextLine();
         }while(!checkInputRange(input, num));
         return Integer.parseInt(input);
     }
 
+    public static String getInput(){
+        Scanner scan = new Scanner(System.in);
+        String input;
+        do{
+            System.out.print("Please enter selection: ");
+            input = scan.nextLine();
+        }while(!checkInput(input));
+        return input;
+    }
+
     public static boolean checkInputRange(String input, int range){
-        boolean valid = checkInput(input);
+        boolean valid = checkIntInput(input);
         if(valid){
             int i = Integer.parseInt(input);
             if (i > range || i <= 0) {
-                notValid();
-                valid = false;
+                notValid(valid);
             }
+        }
+        return valid;
+    }
+
+    public static boolean checkIntInput(String input){
+        boolean valid = true;
+        try {
+            int i = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            notValid(valid);
         }
         return valid;
     }
 
     public static boolean checkInput(String input){
         boolean valid = true;
-        if(input.isEmpty()) {
-            notValid();
-        } else {
-            try {
-                int i = Integer.parseInt(input);
-            } catch (NumberFormatException e) {
-                notValid();
-                valid = false;
+            if(input.isEmpty()){
+                notValid(valid);
             }
-        }
         return valid;
-    }
-
-    public static void notValid(){
-        System.out.println("input is not valid");
     }
 
     public static void populateBookList(){
@@ -95,16 +107,43 @@ public class BibliotecaApp {
     }
 
     public static void checkOutBook(){
-        System.out.println("There are " + numOfBooks + " available.\n"+
-                "please enter a book number.\n");
+        System.out.println("There are " + numOfBooks + " available.\n");
         int i = getInput(numOfBooks);
         if(list.get(i-1).isCheckedOut())
         {
-            System.out.println("This book is already checked out");
+            System.out.println("Sorry, that book is not available");
         }else{
-            System.out.println("Checking out " + list.get(i-1));
+            System.out.println("Thank you! Enjoy the book");
             list.get(i-1).checkOutBook();
             fixList(i-1);
+        }
+    }
+
+    private static void checkInBook() {
+        System.out.println("Please give a Book title");
+        String book;
+        int bookLocation;
+        book = getInput();
+        bookLocation = findBook(book);
+        validateCheckIn(bookLocation);
+    }
+
+    private static int findBook(String book){
+        int found = -1;
+        for (Book i: list) {
+            if(i.isCheckedOut() && i.bookTitle().equals(book)){
+                found = list.indexOf(i);
+            }
+        }
+        return found;
+    }
+
+    public static void validateCheckIn(int bookLocation){
+        if(bookLocation == -1){
+            System.out.println("That is not a valid book to return.");
+        }else{
+            System.out.println("Thank you for returning the book");
+            list.get(bookLocation).checkInBook();
         }
     }
 
@@ -112,6 +151,12 @@ public class BibliotecaApp {
         Book temp = list.get(i);
         list.remove(i);
         list.add(temp);
+    }
+
+    public static boolean notValid(boolean valid){
+        System.out.println("input is not valid");
+        valid = !valid;
+        return valid;
     }
 
     public static void space(){
@@ -132,8 +177,16 @@ class Book{
         yearPublished = i;
     }
 
+    public String bookTitle(){
+        return title;
+    }
+
     public void checkOutBook(){
         checkedOut = true;
+    }
+
+    public void checkInBook(){
+        checkedOut = false;
     }
 
     public boolean isCheckedOut(){
